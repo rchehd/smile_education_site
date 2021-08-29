265e927b387c54a24bea26180a37f9af33b81fb5
roman <romachehdkdk@gmail.com>
2021-08-29 23:17:26 +0200

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Welcome!</title>
+</head>
+<body>
+<?php
+$var='dfsdf';
+$var2="5555";
+echo($var);
+echo($var2);
+?>
+<h1>Welcome to my_host1.com!</h1>
+
+</body>
+</html>
+
