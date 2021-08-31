bbc7f885fe4b3f610ca40506b29efd725fda1892
roman <romachehdkdk@gmail.com>
2021-08-31 22:43:32 +0200

    add register form

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,40 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+
+    <style>
+        @import "style.css";
+
+    </style>
     <title>Welcome!</title>
 </head>
-<body>
-<?php
-$var='my third branches';
-$var2="third!";
-$var34=9;
-echo($var);
-echo($var2);
-?>
-<h1>Welcome to my_host1.com!</h1>
+<body >
+<h1 class="head">Welcome to my_host1.com!</h1>
+<div >
+    <div class="flname" >
+        <input type="submit" value="Sign In" onclick="location.href='index.php'"  class="s1" >
+        <input  type="submit" value="Sign Up"  onclick="location.href='Register.html'" class="s1"><br></br>
+    </div>
+
+    <div id="f1" >
+        <form class="sign_in" >
+            <div class="column">
+                <h2 class="hname">Sign up for free</h2>
+                <input type="email" placeholder="Email*" required ><br></br>
+
+            </div>
+            <div class="column">
+                <input type="password" placeholder="Password*" required >
+
+            <div>
+                <input type="submit" value="Sign In" class="submit">
+            </div>
+
+        </form>
+    </div>
+</div>
+
+
 
 </body>
 </html>
